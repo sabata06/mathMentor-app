6f28993d9f6b50f2a3574c16f33dd483c520b413
sabata06 <m.karaarslan06@outlook.com>
2024-12-16 00:27:17 +0300

    navigation-updated

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/screens/Login";
+import StudentList from "./src/screens/StudentList";
+import StudentDetail from "./src/screens/StudentDetail";
+import AddStudent from "./src/screens/AddStudent";
+import EditStudent from "./src/screens/EditStudent";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="StudentList"
+          component={StudentList}
+          // options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="StudentDetail"
+          component={StudentDetail}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="AddStudent"
+          component={AddStudent}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="EditStudent"
+          component={EditStudent}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
